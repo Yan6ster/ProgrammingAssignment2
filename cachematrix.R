543cbb365fb1e0812002f563b2bd1ebb4259437b
Yan6ster <jyang_tan@hotmail.com>
2015-07-27 02:14:36 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+##main function with arguement x that contains 4 other functions in it
+##
+makeCacheMatrix <- function(x){
+  i <- NULL   ## to ensure initial value is null
+  
+## set function changes value of x in main function 
+  set <- function(y) { 
+    x <<- y   ## value y will substitue value x in makeCacheMatrix
+    i <<- NULL ## value i in main funciton is replaced with NULL
+  }
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+  get <- function() x  ##return value of x from main function
+  setinverse <- function(solve) i <<- solve ##value solve(not calculated value) is stored in variable i and the setinverse 
+  getinverse <- function() i ##return value i that was set in setinverse or in the main function
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)##a list to store all the functions within a function
+  
 }
 
 
-## Write a short comment describing this function
-
+##
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  i <- x$getinverse() ##look up the cache to take the value in it to be placed in i
+  
+  ##if function is used to find whether there is value in i
+  if(!is.null(i)) {
+    message("getting inverse data")  ##display message if value i is not null
+    return(i)  ##return value i
+  }
+  data <- x$get()  ##get the arguement x
+  i <- solve(data, ...) ##use solve to inverse the data
+  x$setinverse(m)
+  i
 }
+
